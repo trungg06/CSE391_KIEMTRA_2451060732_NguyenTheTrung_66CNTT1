b18cd23fb3b3cafb2e38b246647875218ee1ce39
Trung <trung2006hlbg@gmail.com>
2026-06-10 13:50:34 +0700

    [FEATURE] Thêm data.js

---
+++ data/data.js
@@ -0,0 +1,41 @@
+const libraryData = [
+  {
+    STT: "1",
+    "TÊN SẢN PHẨM": "Điện thoại iPhone 15",
+    "DANH MỤC": "Điện thoại",
+    GIÁ: "24.990.000 đ",
+    "TRẠNG THÁI": "Còn hàng",
+  },
+
+  {
+    STT: "2",
+    "TÊN SẢN PHẨM": "Máy tính bảng SamSung Galaxy tab S9",
+    "DANH MỤC": "Máy tính bảng",
+    GIÁ: "18.500.000 đ",
+    "TRẠNG THÁI": "Còn hàng",
+  },
+
+  {
+    STT: "3",
+    "TÊN SẢN PHẨM": "Chuột Logitech MX Master 3S",
+    "DANH MỤC": "Phụ kiện",
+    GIÁ: "2.490.000 đ",
+    "TRẠNG THÁI": "Hết hàng",
+  },
+
+  {
+    STT: "4",
+    "TÊN SẢN PHẨM": "Máy tính xách tay MacBook Air M3",
+    "DANH MỤC": "Laptop",
+    GIÁ: "31.990.000 đ",
+    "TRẠNG THÁI": "Còn hàng",
+  },
+
+  {
+    STT: "5",
+    "TÊN SẢN PHẨM": "Tai nghe Sony WH-1000XM5",
+    "DANH MỤC": "Tai nghe",
+    GIÁ: "7.790.000 đ",
+    "TRẠNG THÁI": "Còn hàng",
+  },
+];
